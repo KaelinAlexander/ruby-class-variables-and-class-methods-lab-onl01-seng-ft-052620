d15e522de5a781f5ce4f28ebdaa85c46b8b5f8da
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-05 13:39:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,15 @@ class Song
 
 @@count = 0
 @@genres = []
+@@artists = []
   
 def initialize(name, artist, genre)
   @@count += 1
   if @@genres!.include?(genre)
     @@genres << genre
+  end
+  if @@artists!.include?(artist)
+    @@artists << genre
 end
 
 def self.count
